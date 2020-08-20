09cde23eea1ae79d27bb9807cb676d2d229e1545
Lamarr <bakerlamarr@gmail.com>
2020-08-20 12:31:02 -0400

    create navigation file and add config file

---
+++ src/components/08-navigation/_nav-primary.jsx
@@ -0,0 +1,20 @@
+const config = {
+  context: "",
+  nav: {
+    search: false,
+    links: {
+      text: "Current section",
+      is_current: true,
+      id: "nav-section-one",
+      links: "&subnav_links",
+      href: "#",
+
+      if_prefix: "basic-",
+      text: "Section",
+      id: "nav-section-two",
+      links3: "*subnav_links",
+      text: " Simple link",
+      href: "javascript:void(0)"
+    }
+  }
+};
